9071a2a4ac63bc48a1acc4d3ca0b74b268a9925e
Nguyen Bao Dai <nguyen.ba.dai@kloon.vn>
2016-09-05 16:14:02 +0700

    DaiNB 20160905 - import plugin robolectric and jacoco

---
+++ app/src/main/java/jenkin/demo/kloon/vn/demojenkin/User.java
@@ -0,0 +1,16 @@
+package jenkin.demo.kloon.vn.demojenkin;
+
+/**
+ * Created by dainb on 9/5/16.
+ */
+public class User {
+    private String _name;
+
+    public String getName() {
+        return _name;
+    }
+
+    public void setName(String value) {
+        _name = value;
+    }
+}
